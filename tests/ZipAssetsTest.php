fe795ca3671f980639b464d0a550b52401630e5c
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2014-07-21 10:29:27 +0200

    Added some unit tests for my own use

---
+++ tests/ZipAssetsTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Craft;
+
+class ZipAssetsTest extends \WebTestCase 
+{
+    
+    function testActionDownload() 
+    {
+    
+        // fetch random assets
+        $criteria = craft()->elements->getCriteria(ElementType::Asset);
+        $criteria->limit = 2;
+        
+        // post asset id and download zip
+        $this->post(
+            str_replace('/admin/', '/', UrlHelper::getActionUrl('zipAssets/download')),
+            array(
+                'filename' => 'testzip',
+                'files' => $criteria->ids()
+            )
+        );
+        
+        // check if we got a zip
+        $this->assertMime(array('application/zip; charset=utf-8'));
+        
+    }
+    
+}
