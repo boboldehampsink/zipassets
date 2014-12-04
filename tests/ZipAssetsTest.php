cd103f3af68ca9be77df58365f3c1d9e60e67435
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2014-12-04 08:46:04 +0100

    Updated Unit Test setup

--- tests/ZipAssetsTest.php
+++ tests/ZipAssetsTest.php
@@ -7,24 +7,9 @@ class ZipAssetsTest extends BaseTest
     public function setUp()
     {
     
-        // PHPUnit complains about not settings this
-        date_default_timezone_set('UTC');
-    
-        // Get dependencies
-        $dir = __DIR__;
-        $map = array(
-            '\\Craft\\ZipAssetsService' => '/../services/ZipAssetsService.php'
-        );
-
-        // Inject them
-        foreach($map as $classPath => $filePath) {
-            if(!class_exists($classPath, false)) {
-                require_once($dir . $filePath);
-            }
-        }
-    
-        // Set components we're going to use
-        $this->setComponent(craft(), 'zipAssets', new ZipAssetsService);
+        // Load plugins
+        $pluginsService = craft()->getComponent('plugins');
+        $pluginsService->loadPlugins();
     
     } 
     
