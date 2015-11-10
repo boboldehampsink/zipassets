a7b7b9c255e31be8932a74cfeeb0b219bc2f8e77
Andrew Welch <andrew@AmbrosiaSW.com>
2015-11-09 22:23:50 -0500

    Don't add button to non-asset fields

--- resources/js/zipassets.js
+++ resources/js/zipassets.js
@@ -2,22 +2,26 @@ $(function(){
 	$(".elementselect").each(function(){
 		var $assetwrapper = $(this);
 		var linkurl = "/actions/zipAssets/download?filename=assets.zip";
+		var foundasset = false;
 		
 		$assetwrapper.find(".element.linked").each(function(){
 			var $assetitem = $(this);			
 			linkurl = linkurl + "&files[]=" + $assetitem.attr('data-id');
+			foundasset = true;
 		})
 		
-		var $a = $("<a class=''>Download All</a>")
-			.attr("href", linkurl)
-			.attr("target", "_blank")
-			.attr("title", "Download")
-			.addClass("btn")
-			.addClass("dashed")
-			.addClass("sharebtn")
-			.addClass("icon")
-			.appendTo($assetwrapper)
-		$this
-			.append($a);
+		if (foundasset) {
+			var $a = $("<a class=''>Download All</a>")
+				.attr("href", linkurl)
+				.attr("target", "_blank")
+				.attr("title", "Download")
+				.addClass("btn")
+				.addClass("dashed")
+				.addClass("sharebtn")
+				.addClass("icon")
+				.appendTo($assetwrapper)
+			$assetwrapper
+				.append($a);
+		}
 	})
 });
